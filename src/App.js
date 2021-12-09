c0fcc17ccd5734f37cd5403ac9dfc9dabe7bdf0f
richasinhaa <richa.sinha0603@gmail.com>
2021-12-09 15:16:12 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Inbox from "./pages/Inbox";
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Inbox/>} />
+        <Route path="about" element={<About/>} />
+        <Route path="contact" element={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Inbox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Inbox = () => {
+  return (    
+      <h1>Welcome to our website!</h1>
+  );
+};
+
+export default Inbox;
